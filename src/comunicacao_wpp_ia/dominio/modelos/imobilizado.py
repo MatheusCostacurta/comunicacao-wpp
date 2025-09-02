af7d178d684202141092a58c05d20eab6dd55c3a
Matheus Costacurta <matheusagriwin@gmail.com>
2025-09-02 16:02:34 -0300

    refactor: :art: Ajustando estrutura do objeto de imobilizado
    Ajustando estrutura do objeto de imobilizado

--- src/comunicacao_wpp_ia/dominio/modelos/imobilizado.py
+++ src/comunicacao_wpp_ia/dominio/modelos/imobilizado.py
@@ -3,8 +3,9 @@ from typing import Optional
 
 class Imobilizado(BaseModel):
     """DTO para representar uma máquina ou imobilizado da fazenda."""
-    id: int = Field(..., description="O ID único da máquina.")
-    nome: str = Field(..., description="O nome ou descrição da máquina.")
+    id: int = Field(..., description="O ID único da máquina.", alias='identificador')
+    nome: str = Field(..., description="O nome ou descrição da máquina.", alias='descricao')
+    ativo: bool = Field(..., description="Indica se o imobilizado está ativo.")
+    
     numero_serie: Optional[str] = Field(default=None, description="O número de série único da máquina.")
-    horimetro_inicio: Optional[float] = Field(default=None, description="O valor inicial do horímetro ou odômetro.")
-    horimetro_fim: Optional[float] = Field(default=None, description="O valor final do horímetro ou odômetro.")
+    horimetro_atual: Optional[str] = Field(default=None, description="O valor atual do horímetro ou odômetro.")
